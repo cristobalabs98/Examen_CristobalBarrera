e7b8d7490d937f49adab420c1a7ae4af67be8ea5
Cristobal.abs <cristobal.abs98@gmail.com>
2024-12-14 03:46:44 -0300

    borrado en cascada

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
   Rails.application.routes.draw do
     devise_for :users
-    get "home/index"
+    devise_scope :user do
+      root to: "devise/sessions#new"
+      resources :home, only: [ :index ]
+      delete "users/:id", to: "users#destroy", as: :destroy_user_profile
+    end
   end
